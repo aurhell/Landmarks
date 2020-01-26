16645923fb258e154de71d3c70280448d8a01d32
Aurélien Girault <me@agirault.com>
2020-01-26 18:56:43 +0100

    step 1: creating and combining views

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,26 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Aurélien Girault on 26/01/2020.
+//  Copyright © 2020 Aurélien Girault. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(
+                Circle().stroke(Color.white, lineWidth: 4)
+            )
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
